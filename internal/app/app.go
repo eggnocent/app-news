96293c1fa882b8cf2cf0b904ea6b6249c78f6d5a
eggnocent <egiwiratamaaa@gmail.com>
2024-12-31 17:00:02 +0700

    Feat - Config fiber

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,8 +3,18 @@ package app
 import (
 	"app-news/config"
 	"app-news/lib/auth"
+	"app-news/lib/middleware"
+	"app-news/lib/pagination"
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/rs/zerolog/log"
 )
 
@@ -15,9 +25,51 @@ func RunServer() {
 		log.Fatal().Msgf("Error connecting to database: %v", err)
 		return
 	}
+
 	// Cloudflare R2
 	cdfR2 := cfg.LoadAwsConfig()
 	_ = s3.NewFromConfig(cdfR2)
 
 	_ = auth.NewJwt(cfg)
+	_ = middleware.NewMiddleware(cfg)
+
+	_ = pagination.NewPagination()
+
+	app := fiber.New()
+	app.Use(cors.New())
+	app.Use(logger.New(logger.Config{
+		Format: "[${time}] ${ip} ${status} - ${latency} ${method} ${path}\n",
+	}))
+
+	_ = app.Group("/api")
+
+	// Router setup
+	go func() {
+		if cfg.App.AppPort == "" {
+			cfg.App.AppPort = os.Getenv("APP_PORT")
+		}
+
+		if cfg.App.AppPort == "" {
+			log.Fatal().Msg("App port is not defined")
+			return
+		}
+
+		err := app.Listen(":" + cfg.App.AppPort)
+		if err != nil {
+			log.Fatal().Msgf("Error starting server: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	<-quit
+
+	log.Info().Msg("Server shutting down in 5 seconds")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := app.ShutdownWithContext(ctx); err != nil {
+		log.Error().Msgf("Error during server shutdown: %v", err)
+	}
 }
