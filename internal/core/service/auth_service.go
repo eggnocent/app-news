1984bf6f9c2d19166fa66994d4d887c1a16e65dd
eggnocent <egiwiratamaaa@gmail.com>
2025-01-03 10:12:35 +0700

    Feat - Login repo and service

---
+++ internal/core/service/auth_service.go
@@ -0,0 +1,71 @@
+package service
+
+import (
+	"app-news/config"
+	"app-news/internal/adapter/repository"
+	"app-news/internal/core/domain/entity"
+	"app-news/lib/auth"
+	"app-news/lib/conv"
+	"context"
+	"time"
+
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var err error
+var code string
+
+type AuthService interface {
+	GetUserByEmail(ctx context.Context, req entity.LoginRequest) (*entity.AccessToken, error)
+}
+
+type authService struct {
+	authRepository repository.AuthRepository
+	cfg            *config.Config
+	jwtToken       auth.Jwt
+}
+
+func NewAuthRepository(authRepository repository.AuthRepository, cfg *config.Config, jwtToken auth.Jwt) AuthService {
+	return &authService{
+		authRepository: authRepository,
+		cfg:            cfg,
+		jwtToken:       jwtToken,
+	}
+}
+
+func (a *authService) GetUserByEmail(ctx context.Context, req entity.LoginRequest) (*entity.AccessToken, error) {
+	result, err := a.authRepository.GetUserByEmail(ctx, req)
+	if err != nil {
+		code = "[SERVICE] GetUserByEmail -1"
+		log.Errorw(code, err)
+	}
+
+	if checkPass := conv.CheckPasswordHash(req.Password, result.Password); !checkPass {
+		code = "[SERVICE] GetUserByEmail -2"
+		log.Errorw(code, "invalid password")
+		return nil, err
+	}
+
+	jwtData := entity.JwtData{
+		UserId: float64(result.ID),
+		RegisteredClaims: jwt.RegisteredClaims{
+			NotBefore: jwt.NewNumericDate(time.Now().Add(time.Hour * 2)),
+			ID:        string(result.ID),
+		},
+	}
+
+	accessToken, expiresAt, err := a.jwtToken.GenerateToken(&jwtData)
+	if err != nil {
+		code = "[SERVICE] GetUserByEmail -3"
+		log.Errorw(code, err)
+		return nil, err
+	}
+
+	resp := entity.AccessToken{
+		AccessToken: accessToken,
+		ExpiresAt:   expiresAt,
+	}
+
+	return &resp, nil
+}
