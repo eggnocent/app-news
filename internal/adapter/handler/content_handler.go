a614bb2bafbe66ff42a45bb9d6ee4fd94de7f11c
eggnocent <egiwiratamaaa@gmail.com>
2025-01-07 13:38:22 +0700

    Feat - add detail and delete content

--- internal/adapter/handler/content_handler.go
+++ internal/adapter/handler/content_handler.go
@@ -4,6 +4,7 @@ import (
 	"app-news/internal/adapter/handler/response"
 	"app-news/internal/core/domain/entity"
 	"app-news/internal/core/service"
+	"app-news/lib/conv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -75,7 +76,57 @@ func (ch *contentHandler) GetContents(c *fiber.Ctx) error {
 }
 
 // GetContentByID implements ContentHandler.
-func (*contentHandler) GetContentByID(c *fiber.Ctx) error {
+func (ch *contentHandler) GetContentByID(c *fiber.Ctx) error {
+	claims := c.Locals("user").(*entity.JwtData)
+	if claims.UserId == 0 {
+		code = "[HANDLER] GetContentsbyID - 1"
+		log.Errorw(code, err)
+		errorResp.Meta.Status = false
+		errorResp.Meta.Message = "Unauthorized access"
+
+		return c.Status(fiber.StatusUnauthorized).JSON(errorResp)
+	}
+
+	idParam := c.Params("contentID")
+	contentID, err := conv.StringtoInt(idParam)
+	if err != nil {
+		code = "[HANDLER] GetContentsbyID - 2"
+		log.Errorw(code, err)
+		errorResp.Meta.Status = false
+		errorResp.Meta.Message = "Invalid content ID"
+
+		return c.Status(fiber.StatusBadRequest).JSON(errorResp)
+	}
+
+	result, err := ch.contentService.GetContentByID(c.Context(), contentID)
+	if err != nil {
+		code = "[HANDLER] GetContentsbyID - 2"
+		log.Errorw(code, err)
+		errorResp.Meta.Status = false
+		errorResp.Meta.Message = err.Error()
+
+		return c.Status(fiber.StatusBadRequest).JSON(errorResp)
+	}
+
+	defaultSuccessResponse.Meta.Status = true
+	defaultSuccessResponse.Meta.Message = "Success"
+
+	respContent := response.ContentResponse{
+		ID:           result.ID,
+		Title:        result.Title,
+		Excerpt:      result.Excerpt,
+		Description:  result.Description,
+		Image:        result.Image,
+		Tags:         result.Tags,
+		Status:       result.Status,
+		CategoryID:   result.CategoryID,
+		CreatedByID:  result.CreatedByID,
+		CreatedAt:    result.CreatedAt.Format(time.RFC3339),
+		CategoryName: result.Category.Title,
+		Author:       result.User.Name,
+	}
+
+	defaultSuccessResponse.Data = respContent
 	panic("unimplemented")
 }
 
@@ -90,8 +141,43 @@ func (*contentHandler) UpdateContent(c *fiber.Ctx) error {
 }
 
 // DeleteContent implements ContentHandler.
-func (*contentHandler) DeleteContent(c *fiber.Ctx) error {
-	panic("unimplemented")
+func (ch *contentHandler) DeleteContent(c *fiber.Ctx) error {
+	claims := c.Locals("user").(*entity.JwtData)
+	if claims.UserId == 0 {
+		code = "[HANDLER] DeleteContent - 1"
+		log.Errorw(code, err)
+		errorResp.Meta.Status = false
+		errorResp.Meta.Message = "Unauthorized access"
+
+		return c.Status(fiber.StatusUnauthorized).JSON(errorResp)
+	}
+
+	idParam := c.Params("contentID")
+	contentID, err := conv.StringtoInt(idParam)
+	if err != nil {
+		code = "[HANDLER] DeleteContent - 2"
+		log.Errorw(code, err)
+		errorResp.Meta.Status = false
+		errorResp.Meta.Message = err.Error()
+
+		return c.Status(fiber.StatusBadRequest).JSON(errorResp)
+	}
+
+	err = ch.contentService.DeleteContent(c.Context(), contentID)
+	if err != nil {
+		code = "[HANDLER] DeleteContent - 3"
+		log.Errorw(code, err)
+		errorResp.Meta.Status = false
+		errorResp.Meta.Message = err.Error()
+
+		return c.Status(fiber.StatusInternalServerError).JSON(errorResp)
+	}
+
+	defaultSuccessResponse.Meta.Status = true
+	defaultSuccessResponse.Meta.Message = "Content deleted successfully"
+	defaultSuccessResponse.Data = nil
+
+	return c.JSON(defaultSuccessResponse)
 }
 
 // UploadImageR2 implements ContentHandler.
--- internal/core/service/content_service.go
+++ internal/core/service/content_service.go
@@ -43,7 +43,14 @@ func (c *contentService) GetContents(ctx context.Context) ([]entity.ContentEntit
 
 // GetContentByID implements ContentService.
 func (c *contentService) GetContentByID(ctx context.Context, id int64) (*entity.ContentEntity, error) {
-	panic("s")
+	result, err := c.contentReport.GetContentByID(ctx, id)
+	if err != nil {
+		code = "[SERVICE] GetContentByID - 1"
+		log.Errorw(code, err)
+		return nil, err
+	}
+
+	return result, nil
 }
 
 // CreateContent implements ContentService.
@@ -58,7 +65,14 @@ func (c *contentService) UpdateContent(ctx context.Context, req entity.ContentEn
 
 // DeleteContent implements ContentService.
 func (c *contentService) DeleteContent(ctx context.Context, id int64) error {
-	panic("unimplemented")
+	err := c.contentReport.DeleteContent(ctx, id)
+	if err != nil {
+		code = "[SERVICE] DeleteContent - 1"
+		log.Errorw(code, err)
+		return err
+	}
+
+	return nil
 }
 
 // UploadImageR2 implements ContentService.
